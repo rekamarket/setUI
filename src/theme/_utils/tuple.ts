1396a55ccc399ae14a330c790c1dec4b9d7caf8b
PolyDevil <polydevil@cocaine.ninja>
2021-05-10 21:14:17 +0300

    code: Add `mixed` tuple utility function

--- src/theme/_utils/tuple.ts
+++ src/theme/_utils/tuple.ts
@@ -1,6 +1,8 @@
 const tuple = <T extends Array<string>>(...args: T) => args
 const tupleNumeric = <T extends Array<number>>(...args: T) => args
+const tupleMixed = <T extends Array<string | number>>(...args: T) => args
 
 tuple.numeric = tupleNumeric
+tuple.mixed = tupleMixed
 
 export default tuple
