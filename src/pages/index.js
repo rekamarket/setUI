faa9d7e9c2641d4770a7e8f0aaea0e524b5dea8f
PolyDevil <polydevil@cocaine.ninja>
2021-05-10 22:20:07 +0300

    code: Update Modal form draft

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,14 @@
-import { Box, Button, ButtonLink, Form, Heading, Input, Modal, Text } from '../units'
+import { Box, Button, ButtonLink, Flex, Form, Heading, Input, Modal, Text } from '../units'
 import { useBoolean } from '../hooks'
 
 export default () => {
   const [isVisible, { setFalse: close, setTrue: open }] = useBoolean(false)
 
   return (
-    <div style={{ display: 'grid', gap: '1rem', background: '#333' }}>
-      <Button onClick={open}>Создать альбом</Button>
+    <main>
+      <Flex margin="large" justifyContent="center">
+        <Button onClick={open}>Создать альбом</Button>
+      </Flex>
 
       <Modal
         visible={isVisible}
@@ -18,47 +20,50 @@ export default () => {
           </Heading>
         }
       >
-        <Form>
-          <Box paddingX="xxlarge" paddingY="xlarge">
-            <Form.Field
-              name="name"
-              label="Имя"
-              rules={[{ required: true, message: 'Введите имя' }]}
-            >
-              <Input
-                theme="primary"
-              />
-            </Form.Field>
+        <Form direction="column" paddingX="xxlarge" paddingY="xlarge">
+          <Form.Field
+            name="name"
+            label="Имя"
+            rules={[{ required: true, message: 'Введите имя' }]}
+          >
+            <Input
+              theme="primary"
+            />
+          </Form.Field>
 
-            <Form.Field
-              name="contacts"
-              label="Контакты"
-              rules={[{ required: true, message: 'Введите контакты' }]}
-            >
-              <Input
-                theme="error"
-              />
-            </Form.Field>
+          <Form.Field
+            name="contacts"
+            label="Контакты"
+            rules={[{ required: true, message: 'Введите контакты' }]}
+          >
+            <Input
+              theme="error"
+            />
+          </Form.Field>
 
-            <Form.Field name="logo" label="Лого">
-              <Input
-                theme="error"
-              />
-            </Form.Field>
+          <Form.Field name="logo" label="Лого">
+            <Input
+              theme="error"
+            />
+          </Form.Field>
 
-            <Box marginTop="xlarge" marginBottom="medium">
-              <div style={{ display: 'flex', gap: '1rem', justifyContent: 'flex-end', alignItems: 'center' }}>
-                <Button theme="primary" variant="transparent" size="small">
-                  Отмена
-                </Button>
-                <Button theme="primary" variant="solid">
-                  Загрузить
-                </Button>
-              </div>
-            </Box>
-          </Box>
+          <Flex
+            marginTop="xlarge"
+            marginBottom="small"
+            gap="medium"
+            justifyContent="flexEnd"
+            alignItems="center"
+          >
+            <Button theme="primary" variant="transparent" size="small">
+              Отмена
+            </Button>
+
+            <Button theme="primary" variant="solid">
+              Загрузить
+            </Button>
+          </Flex>
         </Form>
       </Modal>
-    </div>
+    </main>
   )
 }
