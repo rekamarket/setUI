e3a4784a34f200797da328c4e67f64fa494514d7
PolyDevil <polydevil@cocaine.ninja>
2021-05-10 13:28:42 +0300

    units: Re-export with index file

---
+++ src/units/index.ts
@@ -0,0 +1,6 @@
+export { default as Box } from './Box'
+export { default as Button } from './Button'
+export { default as ButtonLink } from './ButtonLink'
+export { default as Heading } from './Heading'
+export { default as Modal } from './Modal'
+export { default as Text } from './Text'
