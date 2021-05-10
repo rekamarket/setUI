c74d07a29bb8375920f1aa5bd5e796b27a799284
PolyDevil <polydevil@cocaine.ninja>
2021-05-10 13:27:25 +0300

    units: Create `Box` component

---
+++ src/units/Box/types.tsx
@@ -0,0 +1,29 @@
+import type {
+  MarginSizeType,
+  PaddingSizeType,
+  TextAlignType,
+  FontFamilyType,
+  FontSizeType,
+} from '../../theme'
+
+export type Props = {
+  align?: TextAlignType
+  type?: FontFamilyType
+  size?: FontSizeType
+
+  margin?: MarginSizeType
+  marginX?: MarginSizeType
+  marginY?: MarginSizeType
+  marginTop?: MarginSizeType
+  marginRight?: MarginSizeType
+  marginBottom?: MarginSizeType
+  marginLeft?: MarginSizeType
+
+  padding?: PaddingSizeType
+  paddingX?: PaddingSizeType
+  paddingY?: PaddingSizeType
+  paddingTop?: PaddingSizeType
+  paddingRight?: PaddingSizeType
+  paddingBottom?: PaddingSizeType
+  paddingLeft?: PaddingSizeType
+}
