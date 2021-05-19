6321f5091f07badce69a00d48f61fe03f1a8ab59
PolyDevil <polydevil@cocaine.ninja>
2021-05-19 11:02:28 +0300

    note: Add #todo about passing min/max width/height for `<Segment />` component

--- src/layout/Segment/subsets/index.ts
+++ src/layout/Segment/subsets/index.ts
@@ -4,3 +4,6 @@ export { default as Column } from './Column'
 export { default as Row } from './Row'
 
 export default Origin
+
+// #todo: allow to pass min-width/min-height to start/end subcomponents
+// should also depend on the direction - Column/Row
