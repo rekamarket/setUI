e784dfc67a7606e203c955ac6e2d072c48dcff17
PolyDevil <polydevil@cocaine.ninja>
2021-05-30 19:32:40 +0300

    layout: Add `Placeholder` superset to `<Stack />` component

--- src/layout/Stack/index.ts
+++ src/layout/Stack/index.ts
@@ -1,4 +1,13 @@
-export type { Props } from './Component'
-import Stack from './Component'
+import { FC } from 'react'
+import Component, { Props as ComponentProps } from './Component'
+import { Placeholder } from './supersets'
+
+interface I<T = unknown> extends FC<T> {
+  Placeholder: typeof Placeholder
+}
+
+const Stack: I<ComponentProps> = (props) => Component(props)
+
+Stack.Placeholder = Placeholder
 
 export default Stack
