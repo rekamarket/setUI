2dc3a459d3ee5e9ce01a1ebffdcf0fec803860c0
PolyDevil <polydevil@cocaine.ninja>
2021-06-02 10:59:18 +0300

    demo: Add page for `<Stack />` layout component

---
+++ src/pages/layouts/stack/index.tsx
@@ -0,0 +1,18 @@
+import { ButtonLink } from 'button'
+import { Stack, Viewport } from 'layout'
+
+const StackPage = () => {
+  return (
+    <Viewport>
+      <Stack.Placeholder
+        size="xxxlarge"
+        length={5}
+        padding="xxxlarge"
+        margin="xxxlarge"
+        gap="xxxlarge"
+      />
+    </Viewport>
+  )
+}
+
+export default StackPage
