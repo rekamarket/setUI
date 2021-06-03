1710421fecbfc6d96eb4c0a95ad767f263c01bcb
PolyDevil <polydevil@cocaine.ninja>
2021-06-03 08:27:45 +0300

    typography: Extend `<Text />` component tag with `<em />`

--- src/typography/Text/Component/data.ts
+++ src/typography/Text/Component/data.ts
@@ -3,24 +3,24 @@
 	(but we are not gonna do that right now)
 		<blockquote>
 		<dd>
-		<div>
+		// <div>
 		<dt>
 		<figcaption>
 		<figure>
 		<li>
 		<ol>
-		<p>
+		// <p>
 
 		<abbr>
 		<cite>
 		<dfn>
-		<em>
+		// <em>
 		<mark>
 		<q>
 		<samp>
 		<small>
-		<span>
-		<strong>
+		// <span>
+		// <strong>
 		<sub>
 		<sup>
 		<time>
@@ -34,4 +34,5 @@ export const component = {
   div: 'div',
   p: 'p',
   strong: 'strong',
+  em: 'em',
 }
--- src/typography/Text/Component/types.tsx
+++ src/typography/Text/Component/types.tsx
@@ -9,7 +9,7 @@ import type {
   TextTransformType,
 } from 'CSS'
 
-export type AsType = 'span' | 'div' | 'p' | 'strong'
+export type AsType = 'span' | 'div' | 'p' | 'strong' | 'em'
 
 export type Props = {
   as?: AsType
