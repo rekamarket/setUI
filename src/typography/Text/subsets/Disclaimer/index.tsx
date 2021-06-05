e4206b1e50bee8581d3cea3e5eb24d20f1a67a7b
PolyDevil <polydevil@cocaine.ninja>
2021-06-05 23:04:29 +0300

    typography: Restyle `<Text.Disclaimer />`

--- src/typography/Text/subsets/Disclaimer/index.tsx
+++ src/typography/Text/subsets/Disclaimer/index.tsx
@@ -3,7 +3,13 @@ import Component, { Props } from '../../Component'
 
 const Disclaimer: FC<Omit<Props, 'as'>> = (props) =>
   Component({
+    // props default values
+    size: 'smaller',
+
+    // props passed
     ...props,
+
+    // props override
     as: 'small',
   })
 
