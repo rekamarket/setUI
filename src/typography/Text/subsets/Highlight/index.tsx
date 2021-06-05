22d98c7020584cf2bbf207d178152d3dae4c6625
PolyDevil <polydevil@cocaine.ninja>
2021-06-05 23:04:49 +0300

    typography: Restyle `<Text.Highlight />`

--- src/typography/Text/subsets/Highlight/index.tsx
+++ src/typography/Text/subsets/Highlight/index.tsx
@@ -3,7 +3,14 @@ import Component, { Props } from '../../Component'
 
 const Highlight: FC<Omit<Props, 'as'>> = (props) =>
   Component({
+    // props default values
+    background: 'success',
+    color: 'white',
+
+    // props passed
     ...props,
+
+    // props override
     as: 'mark',
   })
 
