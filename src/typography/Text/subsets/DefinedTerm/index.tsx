d12620b996205ac5a8ce3e2f55109dfd163db36d
PolyDevil <polydevil@cocaine.ninja>
2021-06-05 23:04:10 +0300

    typography: Restyle `<Text.DefinedTerm />`

--- src/typography/Text/subsets/DefinedTerm/index.tsx
+++ src/typography/Text/subsets/DefinedTerm/index.tsx
@@ -3,7 +3,13 @@ import Component, { Props } from '../../Component'
 
 const DefinedTerm: FC<Omit<Props, 'as'>> = (props) =>
   Component({
+    // props default values
+    style: 'italic',
+
+    // props passed
     ...props,
+
+    // props override
     as: 'dfn',
   })
 
