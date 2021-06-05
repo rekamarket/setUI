14d2598c5f4ca4241e107f4673a344a5b88881a5
PolyDevil <polydevil@cocaine.ninja>
2021-06-05 23:05:37 +0300

    typography: Restyle `<Text.Subscript />`

--- src/typography/Text/subsets/Subscript/index.tsx
+++ src/typography/Text/subsets/Subscript/index.tsx
@@ -1,10 +1,19 @@
 import { FC } from 'react'
+import cn from 'classnames'
+import { ClassName } from './styles.css'
 import Component, { Props } from '../../Component'
 
-const Subscript: FC<Omit<Props, 'as'>> = (props) =>
+const Subscript: FC<Omit<Props, 'as'>> = ({ className, ...props }) =>
   Component({
+    // props default values
+    size: 'smaller',
+
+    // props passed
     ...props,
+
+    // props override
     as: 'sub',
+    className: cn(ClassName, className),
   })
 
 export default Subscript
