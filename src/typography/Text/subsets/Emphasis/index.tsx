15f74cb92a7c5be3d23d30d1043f36f00342f277
PolyDevil <polydevil@cocaine.ninja>
2021-06-05 23:04:39 +0300

    typography: Restyle `<Text.Emphasis />`

--- src/typography/Text/subsets/Emphasis/index.tsx
+++ src/typography/Text/subsets/Emphasis/index.tsx
@@ -3,7 +3,13 @@ import Component, { Props } from '../../Component'
 
 const Emphasis: FC<Omit<Props, 'as'>> = (props) =>
   Component({
+    // props default values
+    weight: 'bold',
+
+    // props passed
     ...props,
+
+    // props override
     as: 'strong',
   })
 
