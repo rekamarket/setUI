7e4df0d4a51e31fb0fa4f2e2a496c4702cc9abe9
PolyDevil <polydevil@cocaine.ninja>
2021-06-05 23:05:51 +0300

    typography: Restyle `<Text.Superscript />`

--- src/typography/Text/subsets/Superscript/index.tsx
+++ src/typography/Text/subsets/Superscript/index.tsx
@@ -1,10 +1,19 @@
 import { FC } from 'react'
+import cn from 'classnames'
+import { ClassName } from './styles.css'
 import Component, { Props } from '../../Component'
 
-const Superscript: FC<Omit<Props, 'as'>> = (props) =>
+const Superscript: FC<Omit<Props, 'as'>> = ({ className, ...props }) =>
   Component({
+    // props default values
+    size: 'smaller',
+
+    // props passed
     ...props,
+
+    // props override
     as: 'sup',
+    className: cn(ClassName, className),
   })
 
 export default Superscript
