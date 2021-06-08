6f78ef067dd75a8e29dd02abf291e41d05d2db10
PolyDevil <polydevil@cocaine.ninja>
2021-06-08 21:33:31 +0300

    demo: Update `typography/code` page

--- src/pages/typography/code/index.tsx
+++ src/pages/typography/code/index.tsx
@@ -1,43 +1,37 @@
 import { FC } from 'react'
 import { ButtonLink } from 'button'
 import { Code, Heading, LineBreak, Paragraph } from 'typography'
-import Input, { Props } from 'typography/Code/subsets/Input'
 import { Box } from 'layout'
-
-const CustomKey: FC<Props> = (props) =>
-  Input({
-    ...props,
-    background: 'warningPale',
-    borderColor: 'primary',
-    borderRadius: 'medium',
-    borderWidth: 'light',
-    size: 'xxxlarge',
-    style: 'normal',
-    weight: 'bold',
-  })
 
 const Page = () => {
   return (
     <main>
       <Box padding="xxlarge">
-        <Heading level={1}>&lt;code&gt;</Heading>
+        <Heading level={1}>.placeholder - &lt;Code.Placeholder&gt;</Heading>
         <Paragraph>
-          The <Code>push()</Code> method adds one or more elements to the end of
-          an array and returns the new length of the array.
+          The <Code.Placeholder length={23} />
         </Paragraph>
       </Box>
 
       <Box padding="xxlarge">
-        <Heading level={1}>&lt;kbd&gt;</Heading>
+        <Heading level={1}>&lt;code&gt; - &lt;Code.Inline&gt;</Heading>
+        <Paragraph>
+          The <Code.Inline>push()</Code.Inline> method adds one or more elements
+          to the end of an array and returns the new length of the array.
+        </Paragraph>
+      </Box>
+
+      <Box padding="xxlarge">
+        <Heading level={1}>&lt;kbd&gt; - &lt;Code.Input&gt;</Heading>
         <Paragraph>
           Please press <Code.Input>Ctrl</Code.Input> +{' '}
-          <CustomKey>Shift</CustomKey>+ <Code.Input>R</Code.Input> to re-render
-          an MDN page.
+          <Code.Input>Shift</Code.Input>+ <Code.Input>R</Code.Input> to
+          re-render an MDN page.
         </Paragraph>
       </Box>
 
       <Box padding="xxlarge">
-        <Heading level={1}>&lt;samp&gt;</Heading>
+        <Heading level={1}>&lt;samp&gt; - &lt;Code.Output&gt;</Heading>
         <Paragraph>
           <Code.Output>
             Keyboard not found <LineBreak />
@@ -47,7 +41,7 @@ const Page = () => {
       </Box>
 
       <Box padding="xxlarge">
-        <Heading level={1}>&lt;var&gt;</Heading>
+        <Heading level={1}>&lt;var&gt; - &lt;Code.Variable&gt;</Heading>
         <Paragraph>
           The volume of a box is <Code.Variable>l</Code.Variable> ×{' '}
           <Code.Variable>w</Code.Variable> × <Code.Variable>h</Code.Variable>,
@@ -61,3 +55,17 @@ const Page = () => {
 }
 
 export default Page
+
+// import Input, { Props } from 'typography/Code/subsets/Input'
+
+// const CustomKey: FC<Props> = (props) =>
+//   Input({
+//     ...props,
+//     background: 'warningPale',
+//     borderColor: 'primary',
+//     borderRadius: 'medium',
+//     borderWidth: 'light',
+//     size: 'xxxlarge',
+//     style: 'normal',
+//     weight: 'bold',
+//   })
