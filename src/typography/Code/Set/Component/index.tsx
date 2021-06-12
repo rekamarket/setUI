72d0d2e1a8f7cae1f8d9b24ebba327beb693117f
PolyDevil <polydevil@cocaine.ninja>
2021-06-12 14:57:52 +0300

    typography: Re-import `<Code />` source component

---
+++ src/typography/Code/Set/Component/index.tsx
@@ -0,0 +1,88 @@
+import { FC, createElement } from 'react'
+import cn from 'classnames'
+import {
+  Color,
+  Display,
+  TextAlign,
+  FontFamily,
+  FontSize,
+  FontStyle,
+  FontWeight,
+  paddingResolve,
+  BackgroundColor,
+  BorderColor,
+  BorderRadius,
+  BorderWidth,
+} from 'CSS'
+import { PropsRequired } from './types'
+import { component } from './data'
+import { ClassName } from './styles.css'
+
+const Code: FC<PropsRequired> = ({
+  // basic
+  as,
+  className,
+  title,
+
+  // style
+  color,
+  display,
+
+  align,
+
+  type,
+  size,
+  style,
+  weight,
+
+  background,
+
+  borderColor,
+  borderRadius,
+  borderWidth,
+
+  children,
+
+  ...rest
+}) => {
+  const { paddingTop, paddingRight, paddingBottom, paddingLeft } =
+    paddingResolve<Partial<PropsRequired>>(rest)
+
+  return createElement(
+    component[as],
+
+    {
+      className: cn([
+        className,
+        ClassName,
+        Display[display],
+        Color[color],
+
+        TextAlign[align],
+
+        FontFamily[type],
+        FontSize[size],
+        FontStyle[style],
+        FontWeight[weight],
+
+        BackgroundColor[background],
+
+        BorderColor[borderColor],
+        BorderRadius[borderRadius],
+        BorderWidth[borderWidth],
+
+        paddingTop,
+        paddingRight,
+        paddingBottom,
+        paddingLeft,
+      ]),
+      title,
+    },
+
+    children
+  )
+}
+
+export type { Props, PropsRequired, PaddingType } from './types'
+
+export default Code
