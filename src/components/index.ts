b190a47973bbf980b9fdd9a4c699019bde1fb2b4
PolyDevil <polydevil@cocaine.ninja>
2021-06-12 23:53:17 +0300

    code: Re-export components

---
+++ src/components/index.ts
@@ -0,0 +1,19 @@
+// layout
+export { Box } from 'layout'
+export { Container } from 'layout'
+export { Flex } from 'layout'
+export { Segment } from 'layout'
+export { Stack } from 'layout'
+export { Tabs } from 'layout'
+export { Viewport } from 'layout'
+
+// typography
+export { Code } from 'typography';
+export { Edit } from 'typography';
+export { Heading } from 'typography';
+export { LineBreak } from 'typography';
+export { Paragraph } from 'typography';
+export { Quote } from 'typography';
+export { Text } from 'typography';
+export { ThematicBreak } from 'typography';
+export { Time } from 'typography';
