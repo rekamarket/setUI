e2ce0adfcc114f1ec4f321fe265c47bf3b146459
PolyDevil <polydevil@cocaine.ninja>
2021-06-13 14:38:08 +0300

    css: Extend `text-align` property values

--- src/CSS/textAlign/styles.css.ts
+++ src/CSS/textAlign/styles.css.ts
@@ -1,16 +1,48 @@
 import { styleVariants } from '@vanilla-extract/css'
 import { tuple } from 'utils'
 
-export const Keys = ['start', 'end', 'left', 'right', 'center']
+export const Keys = [
+  /* Keyword values */
+  'left',
+  'right',
+  'center',
+  'justify',
+  'justifyAll',
+  'start',
+  'end',
+  'matchParent',
+
+  /* Character-based alignment in a table column */
+  'dot',
+  'dotCenter',
+
+  /* Global values */
+  'inherit',
+  'initial',
+  'unset',
+]
 const VALUES = tuple(...Keys)
 export type Type = typeof VALUES[number]
 
-export const map: Record<Type, any> = {
-  start: 'start',
-  end: 'end',
+export const map: Record<Type, string> = {
+  /* Keyword values */
   left: 'left',
   right: 'right',
   center: 'center',
+  justify: 'justify',
+  justifyAll: 'justify-all',
+  start: 'start',
+  end: 'end',
+  matchParent: 'match-parent',
+
+  /* Character-based alignment in a table column */
+  dot: '"."',
+  dotCenter: '"." center',
+
+  /* Global values */
+  inherit: 'inherit',
+  initial: 'initial',
+  unset: 'unset',
 }
 
 const Styles = styleVariants(map, (value: any) => ({
