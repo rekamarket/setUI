754c2850bce3f5c8c4803f6e9af1aa887ee5146a
PolyDevil <polydevil@cocaine.ninja>
2021-06-15 19:11:29 +0300

    css: Create phantom `background-opacity` property

---
+++ src/CSS/backgroundOpacity/index.ts
@@ -0,0 +1,8 @@
+export {
+  Style as BackgroundOpacity,
+  Keys as BackgroundOpacityKeys,
+  map as BackgroundOpacityMap,
+} from './styles.css'
+
+export type { Type as BackgroundOpacityType } from './styles.css'
+export type { BackgroundOpacityProps } from './types'
