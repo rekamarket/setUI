f1acb46e305da92f91a0721d76e7abfbe3491ac4
PolyDevil <polydevil@cocaine.ninja>
2021-06-23 09:57:41 +0300

    button: Create ProtoComponent

---
+++ src/button/ProtoSet/ProtoComponent/types.tsx
@@ -0,0 +1,32 @@
+import type {
+  RadiusProps,
+  ShapeProps,
+  SizeProps,
+  ThemeProps,
+  VariantProps,
+} from '../css'
+
+interface CustomClickHandler {
+  readonly onClick: () => void
+}
+
+interface FormClickHandler {
+  readonly type: 'reset' | 'submit'
+}
+
+type ClickHandler = CustomClickHandler | FormClickHandler
+
+type BasicProps = {
+  className?: string
+  title?: string
+  disabled?: boolean
+  children: any
+} & ClickHandler
+
+export type StyleProps = RadiusProps &
+  ShapeProps &
+  SizeProps &
+  ThemeProps &
+  VariantProps
+
+export type Props = BasicProps & Required<StyleProps>
