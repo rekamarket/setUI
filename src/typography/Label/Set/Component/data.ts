90f28380cacf6b71bfa95b541fdadc0afa4c3c74
PolyDevil <polydevil@cocaine.ninja>
2021-06-25 15:33:20 +0300

    typography: Remove `<Label />`

--- src/typography/Label/Set/Component/data.ts
+++
@@ -1,4 +0,0 @@
-export const component = {
-  label: 'label',
-  legend: 'legend',
-}
--- src/typography/Label/Set/Component/styles.css.ts
+++
@@ -1,5 +0,0 @@
-import { style } from '@vanilla-extract/css'
-
-export const ClassName = style({
-  margin: 0,
-})
