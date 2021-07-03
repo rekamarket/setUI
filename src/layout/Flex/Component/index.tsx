8958f5fc833918b4168abc8733cd88adedc44da9
PolyDevil <polydevil@cocaine.ninja>
2021-07-03 19:28:20 +0300

    layout: Consume `Block` and `Flex` layers by `<Flex />` component

--- src/layout/Flex/Component/index.tsx
+++ src/layout/Flex/Component/index.tsx
@@ -1,74 +1,15 @@
 import React, { VFC, createElement } from 'react'
 import cn from 'classnames'
-import {
-  // flex
-  AlignContent,
-  AlignItems,
-  FlexDirection,
-  FlexWrap,
-  JustifyContent,
-  // flex values
-  AlignContentMap,
-  AlignItemsMap,
-  FlexDirectionMap,
-  FlexWrapMap,
-  JustifyContentMap,
-
-  // generic
-  backgroundResolve,
-  BackgroundOpacity,
-  BorderColor,
-  BorderRadius,
-  BorderWidth,
-  Gap,
-  MarginBottom,
-  MarginLeft,
-  MarginRight,
-  MarginTop,
-  PaddingBottom,
-  PaddingLeft,
-  PaddingRight,
-  PaddingTop,
-  ZIndex,
-  Width,
-} from 'CSS'
+import { ZIndex, Width } from 'CSS'
+import { BlockLayer, FlexLayer } from 'layers'
 import { Props } from './types'
 import { ClassName } from './styles.css'
 
+const block = new BlockLayer()
+const flex = new FlexLayer()
+
 const Flex: VFC<Props> = ({
   className,
-  // flex
-  alignContent,
-  alignItems,
-  direction,
-  wrap,
-  justifyContent,
-
-  // generic
-  opacity,
-  borderColor,
-  borderRadius,
-  borderWidth,
-
-  gap,
-  gapX,
-  gapY,
-
-  margin,
-  marginX,
-  marginY,
-  marginTop,
-  marginRight,
-  marginBottom,
-  marginLeft,
-
-  padding,
-  paddingX,
-  paddingY,
-  paddingTop,
-  paddingRight,
-  paddingBottom,
-  paddingLeft,
 
   zIndex,
   width,
@@ -77,51 +18,6 @@ const Flex: VFC<Props> = ({
 
   ...rest
 }) => {
-  const resolvedGapX = Gap[gapX] || Gap[gap] || Gap.none
-  const resolvedGapY = Gap[gapY] || Gap[gap] || Gap.none
-
-  const resolvedMarginTop =
-    MarginTop[marginTop] ||
-    MarginTop[marginY] ||
-    MarginTop[margin] ||
-    MarginTop.none
-  const resolvedMarginBottom =
-    MarginBottom[marginBottom] ||
-    MarginBottom[marginY] ||
-    MarginBottom[margin] ||
-    MarginBottom.none
-  const resolvedMarginLeft =
-    MarginLeft[marginLeft] ||
-    MarginLeft[marginX] ||
-    MarginLeft[margin] ||
-    MarginLeft.none
-  const resolvedMarginRight =
-    MarginRight[marginRight] ||
-    MarginRight[marginX] ||
-    MarginRight[margin] ||
-    MarginRight.none
-
-  const resolvedPaddingTop =
-    PaddingTop[paddingTop] ||
-    PaddingTop[paddingY] ||
-    PaddingTop[padding] ||
-    PaddingTop.none
-  const resolvedPaddingBottom =
-    PaddingBottom[paddingBottom] ||
-    PaddingBottom[paddingY] ||
-    PaddingBottom[padding] ||
-    PaddingBottom.none
-  const resolvedPaddingLeft =
-    PaddingLeft[paddingLeft] ||
-    PaddingLeft[paddingX] ||
-    PaddingLeft[padding] ||
-    PaddingLeft.none
-  const resolvedPaddingRight =
-    PaddingRight[paddingRight] ||
-    PaddingRight[paddingX] ||
-    PaddingRight[padding] ||
-    PaddingRight.none
-
   return (
     <div
       className={cn([
@@ -129,33 +25,8 @@ const Flex: VFC<Props> = ({
         className,
         ClassName,
 
-        // flex
-        AlignContent[alignContent] || AlignContent.default,
-        AlignItems[alignItems] || AlignItems.default,
-        FlexDirection[direction] || FlexDirection.default,
-        FlexWrap[wrap] || FlexWrap.default,
-        JustifyContent[justifyContent] || JustifyContent.default,
-
-        // generic
-        backgroundResolve<Partial<Props>>(rest),
-        BackgroundOpacity[opacity] || BackgroundOpacity.default,
-
-        BorderColor[borderColor] || BorderColor.default,
-        BorderRadius[borderRadius] || BorderRadius.default,
-        BorderWidth[borderWidth] || BorderWidth.default,
-
-        resolvedGapX,
-        resolvedGapY,
-
-        resolvedMarginTop,
-        resolvedMarginBottom,
-        resolvedMarginLeft,
-        resolvedMarginRight,
-
-        resolvedPaddingTop,
-        resolvedPaddingBottom,
-        resolvedPaddingLeft,
-        resolvedPaddingRight,
+        block.set(rest).box,
+        flex.set(rest).box,
 
         ZIndex[zIndex] || ZIndex.default,
         Width[width] || Width.default,
--- src/layout/Flex/Component/types.tsx
+++ src/layout/Flex/Component/types.tsx
@@ -1,61 +1,11 @@
-import type {
-  // flex
-  AlignContentType,
-  AlignItemsType,
-  FlexDirectionType,
-  FlexWrapType,
-  JustifyContentType,
-
-  // generic
-  BackgroundProps,
-  BackgroundOpacityProps,
-  BorderColorType,
-  BorderRadiusType,
-  BorderWidthType,
-  GapType,
-  MarginSizeType,
-  PaddingSizeType,
-  ZIndexType,
-  WidthType,
-} from 'CSS'
+import type { ZIndexType, WidthType } from 'CSS'
+import type { BlockLayerProps, FlexLayerProps } from 'layers'
 
 export type Props = {
   children?: JSX.Element | Array<JSX.Element>
   className?: string
 
-  // flex
-  alignContent?: AlignContentType
-  alignItems?: AlignItemsType
-  direction?: FlexDirectionType
-  wrap?: FlexWrapType
-  justifyContent?: JustifyContentType
-
-  // generic
-  borderColor?: BorderColorType
-  borderRadius?: BorderRadiusType
-  borderWidth?: BorderWidthType
-
-  gap?: GapType
-  gapX?: GapType
-  gapY?: GapType
-
-  margin?: MarginSizeType
-  marginX?: MarginSizeType
-  marginY?: MarginSizeType
-  marginTop?: MarginSizeType
-  marginRight?: MarginSizeType
-  marginBottom?: MarginSizeType
-  marginLeft?: MarginSizeType
-
-  padding?: PaddingSizeType
-  paddingX?: PaddingSizeType
-  paddingY?: PaddingSizeType
-  paddingTop?: PaddingSizeType
-  paddingRight?: PaddingSizeType
-  paddingBottom?: PaddingSizeType
-  paddingLeft?: PaddingSizeType
-
   zIndex?: ZIndexType
   width?: WidthType
-} & BackgroundProps &
-  BackgroundOpacityProps
+} & BlockLayerProps &
+  FlexLayerProps
