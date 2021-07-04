81173ca0783121ad6b248ac8f7adf1196cd55524
PolyDevil <polydevil@cocaine.ninja>
2021-07-04 05:21:06 +0300

    layout: Consume layers by `<Container />` component

--- src/layout/Container/Component/types.tsx
+++ src/layout/Container/Component/types.tsx
@@ -1,15 +1,6 @@
 import { tuple } from 'utils'
-import type {
-  AspectRatioType,
-  BackgroundColorType,
-  BorderColorType,
-  BorderRadiusType,
-  BorderWidthType,
-  GapType,
-  MarginSizeType,
-  PaddingSizeType,
-  ZIndexType,
-} from 'CSS'
+import type { AspectRatioType, ZIndexType } from 'CSS'
+import { BlockLayerProps } from 'layers'
 import { DirectionType, MaxWidthType } from '../css'
 
 const Tags = ['div', 'section']
@@ -27,31 +18,5 @@ export type Props = {
   // generic
   aspectRatio?: AspectRatioType
 
-  background?: BackgroundColorType
-
-  borderColor?: BorderColorType
-  borderRadius?: BorderRadiusType
-  borderWidth?: BorderWidthType
-
-  gap?: GapType
-  gapX?: GapType
-  gapY?: GapType
-
-  margin?: MarginSizeType
-  marginX?: MarginSizeType
-  marginY?: MarginSizeType
-  marginTop?: MarginSizeType
-  marginRight?: MarginSizeType
-  marginBottom?: MarginSizeType
-  marginLeft?: MarginSizeType
-
-  padding?: PaddingSizeType
-  paddingX?: PaddingSizeType
-  paddingY?: PaddingSizeType
-  paddingTop?: PaddingSizeType
-  paddingRight?: PaddingSizeType
-  paddingBottom?: PaddingSizeType
-  paddingLeft?: PaddingSizeType
-
   zIndex?: ZIndexType
-}
+} & BlockLayerProps
