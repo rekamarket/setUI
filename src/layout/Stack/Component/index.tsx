52c7fef619a4839a4ce06ad47e06be2b86245dc6
PolyDevil <polydevil@cocaine.ninja>
2021-07-04 05:20:19 +0300

    layout: Consume layers by `<Stack />` component

--- src/layout/Stack/Component/index.tsx
+++ src/layout/Stack/Component/index.tsx
@@ -1,106 +1,24 @@
 import React, { VFC, createElement } from 'react'
 import cn from 'classnames'
-import {
-  AspectRatio,
-  BackgroundColor,
-  BorderColor,
-  BorderRadius,
-  BorderWidth,
-  Gap,
-  MarginBottom,
-  MarginLeft,
-  MarginRight,
-  MarginTop,
-  PaddingBottom,
-  PaddingLeft,
-  PaddingRight,
-  PaddingTop,
-  ZIndex,
-} from 'CSS'
+import { AspectRatio, ZIndex } from 'CSS'
+import { BlockLayer, FlexLayer } from 'layers'
 import { tags } from './data'
 import { Props } from './types'
 import { ClassName } from './styles.css'
+
+const block = new BlockLayer()
+const flex = new FlexLayer()
 
 const Stack: VFC<Props> = ({
   as,
 
   aspectRatio,
 
-  background,
-
-  borderColor,
-  borderRadius,
-  borderWidth,
-
-  gap,
-  gapX,
-  gapY,
-
-  margin,
-  marginX,
-  marginY,
-  marginTop,
-  marginRight,
-  marginBottom,
-  marginLeft,
-
-  padding,
-  paddingX,
-  paddingY,
-  paddingTop,
-  paddingRight,
-  paddingBottom,
-  paddingLeft,
-
   zIndex,
 
   children,
+  ...rest
 }) => {
-  const resolvedGapX = Gap[gapX] || Gap[gap] || Gap.none
-  const resolvedGapY = Gap[gapY] || Gap[gap] || Gap.none
-
-  const resolvedMarginTop =
-    MarginTop[marginTop] ||
-    MarginTop[marginY] ||
-    MarginTop[margin] ||
-    MarginTop.none
-  const resolvedMarginBottom =
-    MarginBottom[marginBottom] ||
-    MarginBottom[marginY] ||
-    MarginBottom[margin] ||
-    MarginBottom.none
-  const resolvedMarginLeft =
-    MarginLeft[marginLeft] ||
-    MarginLeft[marginX] ||
-    MarginLeft[margin] ||
-    MarginLeft.none
-  const resolvedMarginRight =
-    MarginRight[marginRight] ||
-    MarginRight[marginX] ||
-    MarginRight[margin] ||
-    MarginRight.none
-
-  const resolvedPaddingTop =
-    PaddingTop[paddingTop] ||
-    PaddingTop[paddingY] ||
-    PaddingTop[padding] ||
-    PaddingTop.none
-  const resolvedPaddingBottom =
-    PaddingBottom[paddingBottom] ||
-    PaddingBottom[paddingY] ||
-    PaddingBottom[padding] ||
-    PaddingBottom.none
-  const resolvedPaddingLeft =
-    PaddingLeft[paddingLeft] ||
-    PaddingLeft[paddingX] ||
-    PaddingLeft[padding] ||
-    PaddingLeft.none
-  const resolvedPaddingRight =
-    PaddingRight[paddingRight] ||
-    PaddingRight[paddingX] ||
-    PaddingRight[padding] ||
-    PaddingRight.none
-
   return createElement(
     tags[as] || tags.div,
 
@@ -112,24 +30,8 @@ const Stack: VFC<Props> = ({
         // generic
         AspectRatio[aspectRatio] || AspectRatio.default,
 
-        BackgroundColor[background] || BackgroundColor.default,
-
-        BorderColor[borderColor] || BorderColor.default,
-        BorderRadius[borderRadius] || BorderRadius.default,
-        BorderWidth[borderWidth] || BorderWidth.default,
-
-        resolvedGapX,
-        resolvedGapY,
-
-        resolvedMarginTop,
-        resolvedMarginBottom,
-        resolvedMarginLeft,
-        resolvedMarginRight,
-
-        resolvedPaddingTop,
-        resolvedPaddingBottom,
-        resolvedPaddingLeft,
-        resolvedPaddingRight,
+        block.set(rest).box,
+        flex.set(rest).box,
 
         ZIndex[zIndex] || ZIndex.default,
       ]),
--- src/layout/Stack/Component/types.tsx
+++ src/layout/Stack/Component/types.tsx
@@ -1,15 +1,6 @@
 import { tuple } from 'utils'
-import type {
-  AspectRatioType,
-  BackgroundColorType,
-  BorderColorType,
-  BorderRadiusType,
-  BorderWidthType,
-  GapType,
-  MarginSizeType,
-  PaddingSizeType,
-  ZIndexType,
-} from 'CSS'
+import type { AspectRatioType, ZIndexType } from 'CSS'
+import type { BlockLayerProps, FlexLayerProps } from 'layers'
 
 const Tags = ['div', 'ul', 'li', 'dl']
 const TAGS = tuple(...Tags)
@@ -21,31 +12,6 @@ export type Props = {
 
   aspectRatio?: AspectRatioType
 
-  background?: BackgroundColorType
-
-  borderColor?: BorderColorType
-  borderRadius?: BorderRadiusType
-  borderWidth?: BorderWidthType
-
-  gap?: GapType
-  gapX?: GapType
-  gapY?: GapType
-
-  margin?: MarginSizeType
-  marginX?: MarginSizeType
-  marginY?: MarginSizeType
-  marginTop?: MarginSizeType
-  marginRight?: MarginSizeType
-  marginBottom?: MarginSizeType
-  marginLeft?: MarginSizeType
-
-  padding?: PaddingSizeType
-  paddingX?: PaddingSizeType
-  paddingY?: PaddingSizeType
-  paddingTop?: PaddingSizeType
-  paddingRight?: PaddingSizeType
-  paddingBottom?: PaddingSizeType
-  paddingLeft?: PaddingSizeType
-
   zIndex?: ZIndexType
-}
+} & BlockLayerProps &
+  FlexLayerProps
