d7897a6d6361cae1f17b85b7c4ac42b543d01c41
Poly Devil <polydevil@cocaine.ninja>
2021-11-18 18:23:26 +0300

    update index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,13 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 
 const IndexPage: NextPage = () => (
-  <a href="/marketplacezametallom">marketplacezametallom</a>
+  <>
+    <a href="/marketplacezametallom">marketplacezametallom</a>
+    <br />
+    <a href="/development">development</a>
+    <br />
+    <a href="/b2bmarketplace">b2bmarketplace</a>
+  </>
 )
 
 export default IndexPage
