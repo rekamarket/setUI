5e20de339f2d6e5b694648f2e0c5a275dac06560
Poly Devil <polydevil@cocaine.ninja>
2021-11-19 12:24:34 +0300

    code: add index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,51 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import { Frame } from '../src/layout'
+import { H1, H2, Paragraph, Time } from 'typography';
+
+import classes from './index.module.css';
+
+const list = [
+  {
+    key: 'b2bmarketplace',
+    title: 'B2B маркетплейсы: сценарии развития, убьют ли маркетплейсы бизнес трейдеров?',
+    description: 'Металлургические компании смогли достичь успеха благодаря масштабному внедрению цифровых технологий и аналитики в свою сферу деятельности',
+    date: '01.10.2021',
+    link: '/b2bmarketplace',
+  },
+  {
+    key: 'development',
+    title: 'Изменение строительных требований',
+    description: 'С 1 сентября 2021 года количество обязательных требований в сфере строительстве сократилось более чем на 50% (на 3,8 тыс. пунктов)',
+    date: '27.09.2021',
+    link: '/development',
+  },
+  {
+    key: 'marketplacezametallom',
+    title: 'Как меняются цены на рынке металлопроката',
+    description: 'Состояние рынка стали является побочным продуктом ряда внутренних и международных событий. Это проблема не только России, с ней сталкивается каждый бизнес и каждый потребитель во всём мире',
+    date: '27.09.2021',
+    link: '/marketplacezametallom',
+  },
+];
 
 const IndexPage: NextPage = () => (
-  <>
-    <a href="/marketplacezametallom">marketplacezametallom</a>
-    <br />
-    <a href="/development">development</a>
-    <br />
-    <a href="/b2bmarketplace">b2bmarketplace</a>
-  </>
+  <Frame charsPerLine="large">
+    <H1 textAlign="center">Свежие статьи</H1>
+
+    <ul className={classes.list}>
+      {list.map(e => (
+        <li key={e.key} className={classes.item}>
+          <article className={classes.article}>
+            <H2 className={classes.title}>{e.title}</H2>
+            <Paragraph className={classes.description}>{e.description}</Paragraph>
+            <Time className={classes.date}>{e.date}</Time>
+            <a href={e.link} className={classes.link + " " + "sr-only"}>{e.title}</a>
+          </article>
+        </li>
+      ))}
+    </ul>
+  </Frame>
 )
 
 export default IndexPage
