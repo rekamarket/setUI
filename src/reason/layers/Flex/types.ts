7787e4629624338844efaaf0c643726cf36a0692
Poly Devil <polydevil@cocaine.ninja>
2021-11-23 10:11:21 +0300

    code: autoformat

--- src/reason/layers/Flex/types.ts
+++ src/reason/layers/Flex/types.ts
@@ -5,4 +5,9 @@ import { FlexWrap_t } from './FlexWrap/FlexWrap.css.gen'
 import { Gap_t } from './Gap/Gap.css.gen'
 import { JustifyContent_t } from './JustifyContent/JustifyContent.css.gen'
 
-export type Props = AlignContent_t & AlignItems_t & FlexDirection_t & FlexWrap_t & Gap_t & JustifyContent_t;
+export type Props = AlignContent_t &
+  AlignItems_t &
+  FlexDirection_t &
+  FlexWrap_t &
+  Gap_t &
+  JustifyContent_t
