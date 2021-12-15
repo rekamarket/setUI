a5a47f32b76f299479d420a5f0db83f96eb3d59e
Poly Devil <polydevil@cocaine.ninja>
2021-12-15 15:16:28 +0300

    types: Allow `<Heading />` to accept children as `ReactNode`

--- src/typography/Heading/ProtoSet/Component/types.ts
+++ src/typography/Heading/ProtoSet/Component/types.ts
@@ -11,6 +11,15 @@ import type {
 export const LevelKeys = [1, 2, 3, 4, 5, 6]
 export type LevelType = 1 | 2 | 3 | 4 | 5 | 6
 
+type ReactNode =
+  | React.ReactChild
+  | React.ReactNodeArray
+  | ReadonlyArray<ReactNode>
+  | React.ReactPortal
+// | boolean
+// | null
+// | undefined;
+
 export interface Props
   extends ICharsPerLineLayer,
     IContentLayer,
@@ -22,5 +31,5 @@ export interface Props
   level: LevelType
   className?: string
   title?: string
-  children: string
+  children: ReactNode
 }
