6c0fe578bf34171e620098dbafacbb3666445c56
Poly Devil <polydevil@cocaine.ninja>
2021-12-20 10:18:05 +0300

    typography: Add displayName to `<LineBreak />` typography component

--- src/typography/LineBreak/index.ts
+++ src/typography/LineBreak/index.ts
@@ -1,5 +1,9 @@
 import { VFC, createElement } from 'react'
 
+export const displayName = 'LineBreak'
+
 const LineBreak: VFC = () => createElement('br')
+
+LineBreak.displayName = displayName
 
 export default LineBreak
